b3a1602921f0742656d690890a9ca5dcffdc7ec3
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-07-27 22:56:21 +0100

    "fixed the profile image error"

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -14,12 +14,12 @@ const Landing = () => {
                 <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-20 before:rounded-t-[400px]
                 before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]">
 
-                    <img src="src/assets/profilepic.png" alt="profile"
+                    <img src="/assets/profilepic.png" alt="profile"
                     className="filter saturate-50 hover:saturate-100 transition duration-1000 z-10 w-full
                      max-w-[400px] md:max-w-[600px]" />
                 </div>
             ) : (
-                <img src="src/assets/profilepic.png" alt="profile"
+                <img src="assets/profilepic.png" alt="profile"
                     className="filter saturate-50 hover:saturate-100 transition duration-500 z-10 w-full
                      max-w-[400px] md:max-w-[600px]" />
             )}
